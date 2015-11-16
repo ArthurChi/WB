36369529e4e0a44b0217eb3a64645c2ed2c7db00
cjfire <cjfire@sina.cn>
2015-11-16 14:35:09 +0800

    一些力气活, 创建SB以及Controller

---
+++ WeiBo/WeiBo/Classes/Sections/Profile/ProfileViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ProfileViewController.swift
+//  WeiBo
+//
+//  Created by cjfire on 15/11/16.
+//  Copyright © 2015年 Cjfire. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: VistorViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
