86646b4740391c1bfd9593a259bd9271feef54c9
Cjfire <cjfire@sina.cn>
2015-11-26 14:46:52 +0800

    非转发cell完成

--- WeiBo/WeiBo/Classes/Sections/Home/View/NormalCell.swift
+++ WeiBo/WeiBo/Classes/Sections/Home/View/NormalCell.swift
@@ -10,12 +10,23 @@ import UIKit
 
 class NormalCell: HomeViewCell {
 
+    override var statusViewModel: StatusViewModel? {
+        didSet {
+            
+            pictureShowView.snp_updateConstraints { (make) -> Void in
+                
+                let count = (statusViewModel?.statusModel?.thumbnailUrls.count ?? 0)
+                make.top.equalTo(contentLabel.snp_bottom).offset(count > 0 ? StatusCellMargin:0)
+            }
+        }
+    }
+    
     override func setupUI() {
         super.setupUI()
         
         pictureShowView.snp_makeConstraints { (make) -> Void in
             make.left.equalTo(self).offset(StatusCellMargin)
-            make.top.equalTo(contentLabel.snp_bottom)
+            make.top.equalTo(contentLabel.snp_bottom).offset(StatusCellMargin)
             make.width.equalTo(300)
             make.height.equalTo(90)
         }
